702951239a65fdbb540bf5693aa816ac371a5ba3
garyCC227 <z5163479@student.unsw.edu.au>
2021-01-10 22:38:29 +1100

    Updates

---
+++ precache-manifest.2abd77578bfddf026daa50bf6b88c660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c7c3800de95c73e203bf727c302a4f",
+    "url": "/blog-gary/index.html"
+  },
+  {
+    "revision": "ef2351517e780d54079e",
+    "url": "/blog-gary/static/css/main.342faae2.chunk.css"
+  },
+  {
+    "revision": "4f06bc386caeb8022d4a",
+    "url": "/blog-gary/static/js/2.a26ed993.chunk.js"
+  },
+  {
+    "revision": "f4397f88bfd4c390e3817518294be012",
+    "url": "/blog-gary/static/js/2.a26ed993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef2351517e780d54079e",
+    "url": "/blog-gary/static/js/main.69fda98e.chunk.js"
+  },
+  {
+    "revision": "f18356161b1de479b760",
+    "url": "/blog-gary/static/js/runtime-main.4b1f349f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9710cd1b432e309109a8c69debe97ad1.js"
+  "/blog-gary/precache-manifest.2abd77578bfddf026daa50bf6b88c660.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blog-gary/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
