78b5aaf4f7a2e7d4c032eb4f32cd368af65e5247
garyCC227 <z5163479@student.unsw.edu.au>
2021-01-15 20:30:49 +1100

    todo list

--- blog-app/src/components/CardList/CardList.jsx
+++ blog-app/src/components/CardList/CardList.jsx
@@ -5,33 +5,6 @@ import Card from '../Card';
 import {FilterContext} from "../../utils/store";
 import styles from './CardList.module.css';
 
-
-/*
- {
-   'ID1':{
-    "id": 1590948900,
-    "title": "How to hide your API keys in React.\r",
-    "author": {
-      "owen":'url1',
-    },
-    "lastUpdateTime":"string",
-    "date": "Jun 01 2020\r",
-    "tag": ["api\r", 'react'],
-    "filePath":'post1.md'
-  },
-   'ID2'
- }
-
- 4. about me page template
-   1. about me 
-   2. experience
-   4. contact page
- 5.recent work
- 6. dark/light mode
-
-
-*/
-
 const CardList = ({blogs, tagsMeta}) => {
   //blogs are all blogs
   const n = 4 //shown post end at filteredblogs; n is how many more blogs we show when we loadmore
