f0afb92be004d46c23b99abc909cf2109c624de3
garyCC227 <z5163479@student.unsw.edu.au>
2021-01-24 00:01:05 +1100

    update more

--- blog-app/src/utils/convertMD.py
+++ blog-app/src/utils/convertMD.py
@@ -1,24 +1,54 @@
 import json
 
-file = r"../content/Spring_RESTful.md"
-with open(file,'r') as f:
+'''
+ Usage:
+   1. Modify the wanted .md file name
+   2. Modify the blog meta data 
+   3. run with Python3 convertMD.py
+   4. Done, check you new blog in the app
+'''
+
+
+##configure the blog meta-data
+# read the md content into oneline string
+file = r"../content/Spring_RESTful.md" #TODO: 1. Modify file name
+with open(file,'r', encoding='utf-8') as f:
     output = f.read()
     content = rf'{output}'
 
-print(content, end="")
+#Blog meta data. TODO: 2. modify this everytime
+new_blog = {
+  "id":1, #int
+  "title":"RESTful API with Spring framework", #string
+  "author":"Linchen Chen", #string
+  "date":"Jan 16 2021", #string
+  "tag":["API"],  #array
+  "content":content #string
+}
 
+# print(content, end="")
+
+
+#read the old database
 data_path = r"../cardNote.json"
-with open(data_path, 'rb') as f:
+with open(data_path, 'r') as f:
   data = json.loads(f.read())
 
-request_id = 1
+# update database
 blogs = data['blogs']
+existed = False
 for index, blog in enumerate(blogs):
-  if request_id == blog["id"]:
-    blogs[index]["content"] = content
+  #update existed blog
+  if new_blog["id"] == blog["id"]:
+    blogs[index] = new_blog
+    existed = True
+    break
 
-data['blogs'] = blogs
+if existed:
+  data['blogs'] = blogs
+else:
+  #if it's new blog
+  data['blogs'].append(new_blog)
 
-
-with open(data_path, 'w',  encoding='utf-8') as f:
+with open(data_path, 'w') as f:
   json.dump(data, f, indent=4)
--- blog-app/src/utils/store.js
+++ blog-app/src/utils/store.js
@@ -7,8 +7,11 @@ export const FilterContext = React.createContext(null)
 export default ({ children }) => {
   const [selectedTags, setSelectedTags] = React.useState(new Set(Object.keys(data.tags)));
   const [blogCount, setBlogCount] = React.useState(data.blogs.length);
-  const [filteredBlogs, setFilteredBlogs] = React.useState(data.blogs);
+  const blogs = data.blogs;
+  blogs.sort(sortByDateString)
+  const [filteredBlogs, setFilteredBlogs] = React.useState(blogs);
 
+  
   const store = {
     tags: [selectedTags, setSelectedTags],
     blogCount: [blogCount, setBlogCount],
@@ -16,4 +19,11 @@ export default ({ children }) => {
   }
 
   return <FilterContext.Provider value={store}>{children}</FilterContext.Provider>
+}
+
+const sortByDateString = (blog1, blog2) => {
+  const time1 = new Date(blog1.date);
+  const time2 = new Date(blog2.date);
+  
+  return time1 - time2;
 }
