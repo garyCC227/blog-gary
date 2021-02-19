ee3a7892051176d8a2971414542c6c0062c46b5a
oujianjiu <oujianjiu@gmail.com>
2021-02-19 15:36:10 +1100

    read db from filepath

--- blog-app/src/pages/post.js
+++ blog-app/src/pages/post.js
@@ -5,7 +5,7 @@ import Markdown from "react-markdown";
 import hljs from "highlight.js";
 import SyncLoader from "react-spinners/SyncLoader";
 import { css } from "@emotion/core";
-import {DiscussionEmbed} from "disqus-react";
+import { DiscussionEmbed } from "disqus-react";
 
 
 import "./post.css";
@@ -30,24 +30,25 @@ const Post = (props) => {
     title: 'comments'
   })
 
-
   useEffect(() => {
     const validId = parseInt(props.match.params.id);
-    
-    if(posts.blogs[`${validId}`]!== undefined){
-      setPost(posts.blogs[`${validId}`]);
 
-      
+    if (posts.blogs[`${validId}`] !== undefined) {
+      let post = posts.blogs[`${validId}`];
+      let file = post['file'];
+      const path = require(`../content/${file}`);
+      fetch(path)
+        .then(res => res.text())
+        .then(content => {
+          post.content = content
+          setPost(post);
+        })
     };
-    
-    // app.database().ref('blogs')
-    //   .get() 
-    //   .then(res => setPosts(res.val()['blogs']))
 
   }, [])
 
   useEffect(() => {
-    if(post !== null){
+    if (post !== null) {
       document.querySelectorAll("pre code").forEach(block => {
         hljs.highlightBlock(block);
       });
@@ -68,36 +69,36 @@ const Post = (props) => {
 
   return (
     <>
-    { post === null ?
-      <SyncLoader color={'#9afcf1'} loading={true} css={override} size={40} />
-    :(<div>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>{post.title}</title>
-      </Helmet>
-      <div className="blog-container">
-        <div className="blog-body">
-          <div className="post-body">
-            <div className="post-head">
-              <h2>{post.title}</h2>
-              <p>
-                {post.date} by <span>Linchen Chen</span>
-              </p>
+      {post === null ?
+        <SyncLoader color={'#9afcf1'} loading={true} css={override} size={40} />
+        : (<div>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>{post.title}</title>
+          </Helmet>
+          <div className="blog-container">
+            <div className="blog-body">
+              <div className="post-body">
+                <div className="post-head">
+                  <h2>{post.title}</h2>
+                  <p>
+                    {post.date} by <span>Linchen Chen</span>
+                  </p>
+                </div>
+                <Markdown className="post-markdown" children={post.content} escapeHtml={false} />
+              </div>
             </div>
-            <Markdown className="post-markdown" children={post.content} escapeHtml={false} />
-          </div>
-        </div>
 
-      </div>
-      { disqusConfig.identifier !== null &&
-        
-          
-          <DiscussionEmbed shortname={disqusShortname} config={disqusConfig}/>
-        /* <CommentBox  blogId={validId} db={post.AllComments} rootCommentsIds={post.rootCommentIDs} /> */
-        
+          </div>
+          {disqusConfig.identifier !== null &&
+
+
+            <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+            /* <CommentBox  blogId={validId} db={post.AllComments} rootCommentsIds={post.rootCommentIDs} /> */
+
+          }
+        </div>)
       }
-    </div>)
-    }
     </>
   );
 };
